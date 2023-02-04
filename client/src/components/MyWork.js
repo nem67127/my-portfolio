0c1ef45cc225ee0b7fafaab06a5a99b6a089034c
Natasha Moses <nem67127@gmail.com>
2023-02-03 18:38:11 -0800

    styled projects with styled components

--- client/src/components/MyWork.js
+++ client/src/components/MyWork.js
@@ -1,23 +1,23 @@
 
 import { useEffect, useState } from "react";
-import madeLocallyImage from "../photos/madeLocally.png";
 import styled from "styled-components";
 
 const MyWork = () => {
 
     //fetch projects from MongoDB and set them inside
     const [projects, setProjects] = useState("");
+
     //useEffect to fetch projects
     useEffect(()=>{
         fetch(`http://localhost:8000/projects`)
             .then((res) => res.json())
             .then((data) => {
-                console.log(data.data);
                 setProjects(data.data);
         }).catch((err)=>{
             console.log(err);
         })  
     },[])
+
     
 
 return (
@@ -26,26 +26,21 @@ return (
     <Wrapper>
         <Title>MY WORK</Title>
         <div className="big-section carousel" >
-            {/* will map over projects to create cards */}
             <div className="inner-carousel">
-                < div className="card start">
-                    <div className="img-wrapper">
-                        <img className="img-mywork" src={madeLocallyImage} alt="MadeLocally Homepage"/>
-                    </div>
-                    <div className="img-description">
-                        <h2>MadeLocally</h2>
-                        <p>A Local Artisan Directory. This is my final project as part of Concordia Univeristy's Full Stack Web Development Bootcamp.</p>
-                    </div>
-                </div>
-                <div className="card">
-                <div className="img-wrapper">
-                        <img className="img-mywork" src="" alt="Ecommerce Store"/>
-                    </div>
-                    <div className="img-description">
-                        <h2>Ecommerce store</h2>
-                        <p>Group Project of an E-commerce store as part of the curriculum of Concordia Univeristy's Full Stack Web Development Bootcamp.</p>
-                    </div>
-                </div>
+                {projects.length > 0 ? projects.map((project)=>{
+                    console.log(project)
+                    return (
+                    <Project key={project.name}>
+                        <ImgWrapper>
+                        <Img className="img-mywork" src={require(`../photos/${project.photo}`)} alt={project.name}/>
+                        </ImgWrapper>
+                        <InfoBox>
+                            <h2>{project.name}</h2>
+                            <p>{project.description}</p>
+                            <a href={project.link}>link to project</a>
+                        </InfoBox>
+                    </Project>)
+                }): <p>loading</p>}
             </div>
         </div>
 
@@ -69,3 +64,53 @@ const Title = styled.h2`
     display: flex;
     align-items: center;
 `;
+
+const InfoBox = styled.div`
+    visibility: hidden;
+    position: absolute;
+    width: 100%;
+    min-height: 18vh;
+    top: 72%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+    background-color: rgba(255, 255, 255, 0.6);
+    padding: 2vh;
+`;
+
+const Project = styled.div`
+    margin-right: 7.5vw;
+    width: 80vw;
+    height: 95%;
+    background: linear-gradient(217deg, rgba(237,240,241, 0.8), rgba(187, 227, 217, 0) 70.71%),
+            linear-gradient(336deg, rgba(90,173,125, 0.8), rgba(178, 203, 178, 0) 70.71%),
+            linear-gradient(140deg, rgba(199,219,229, 0.8), rgba(157, 157, 167, 0) 70.71%);
+    box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
+    display: flex;
+    flex-direction: column;
+    align-items: center; 
+    position:relative;
+
+    &:hover ${InfoBox} {
+    visibility: visible;
+    };
+
+    &:nth-child(1) {
+        margin-left: 7.5vw;
+    }
+
+`;
+
+const ImgWrapper = styled.div`
+    height: 100%;
+    display:flex;
+    justify-content: center;
+`;
+
+const Img = styled.img`
+    object-fit: scale-down;
+    height: 90%;
+    align-self: center;
+`;
+
